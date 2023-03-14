b55556eb8fa30f9e18f00e7639d352816368d1c9
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-03-14 13:37:37 +0200

    add simple header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import Header from './Header/Header';
 
 export const App = () => {
+  const root = document.querySelector('#root');
+  root?.classList.add('dark');
   return (
-    <>
+    <div className="dark:bg-slate-700 h-screen">
       <Header />
-    </>
+    </div>
   );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { SunIcon, MoonIcon } from '@heroicons/react/24/solid';
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className="flex items-center justify-center w-full font-bold h-20 bg-orange-300">
-      Header
+    <header className="flex dark:text-white dark:bg-slate-900 dark:shadow-white items-center justify-between p-5 md:p-10 lg:max-w-5xl xl:max-w-7xl border-b-2 border-slate-100 rounded-b-2xl shadow-lg m-auto font-bold h-20 bg-slate-300">
+      <MoonIcon className="w-8 cursor-pointer animate-pulse" />
+      <h1>Header</h1>
+      <SunIcon className="w-8 cursor-pointer animate-pulse" />
     </header>
   );
 };
