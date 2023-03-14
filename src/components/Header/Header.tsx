b8ed3e0743cde3503d91b98e6afad5401724c37d
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-03-14 12:53:22 +0200

    test

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header className="flex items-center justify-center w-full font-bold h-20 bg-orange-300">
+      Header
+    </header>
+  );
+};
+
+export default Header;
